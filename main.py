8c74e996ffeddd0ac8eb0f0d3b6d47f97acf68c8
isackpark <charlesisaya142@gmail.com>
2026-08-23 01:18:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -329,12 +329,11 @@ if __name__ == "__main__":
     print()
 
 
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+
     app.run(
-
-        host="127.0.0.1",
-
-        port=5000,
-
-        debug=True
-
+        host="0.0.0.0",
+        port=port,
+        debug=False
     )
